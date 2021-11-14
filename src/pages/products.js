877a8529172cff285872ab3b957772e1425c7535
jianyi0512 <nabara@laguna-hills.co.jp>
2021-11-14 23:15:32 +0900

    3rdEdit

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,5 +1,6 @@
 import Layout from '../components/layout'
 import Link from 'next/link'
+import Image from 'next/image'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Products = () => {
@@ -14,7 +15,8 @@ const Products = () => {
 
           <div className="col">
             <div className="card h-100">
-              <img src="/img/dataton/dataton.png" className="card-img-top" alt="dataton logo"/>
+              {/* <img src="/img/dataton/dataton.png" className="card-img-top" alt="dataton logo"/> */}
+              <Image src="/img/dataton/dataton.png" className="card-img-top" alt="dataton logo" width={221} height={72} quality={90} />
               <div className="card-body">
                 <h5 className="card-title">Dataton<span className="flag-icon flag-icon-se ms-2"></span></h5>
                 <p className="card-text">WATCHOUTは、マルチプロジェクションを行うソフトウェアです。<br/>世界的にも数々のイベントや商業施設で使用されています。<br/>ビデオ編集ソフトと変わらない操作で扱いやすいソフトウェアです。</p>
